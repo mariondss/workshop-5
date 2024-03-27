d78d24f9806ae54998ecca4e6ecd2dbdce55d072
Marion DE SOUSA <marion.de_sousa@edu.devinci.fr>
2024-03-27 10:17:01 +0100

    update

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -45,63 +45,6 @@ export async function node(
   let messagesR = new Map();
   let messagesP = new Map();
 
-  node.post("/message", async (req, res) => {
-    let { k, x, messageType } = req.body;
-    if (!isFaulty && !nodeState.killed) {
-      if (messageType == "R") {
-        if (!messagesR.has(k)) {
-          messagesR.set(k, []);
-        }
-        messagesR.get(k)!.push(x);
-        let messageR = messagesR.get(k)!;
-        if (messageR.length >= (N - F)) {
-          let count0 = messageR.filter((el:number) => el == 0).length;
-          let count1 = messageR.filter((el:number) => el == 1).length;
-          let newX = "?";
-          if (count0 > (N / 2)) {
-            newX = "0";
-          } else if (count1 > (N / 2)) {
-            newX = "1";
-          }
-          for (let i = 0; i < N; i++) {
-            axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k, x: newX, messageType: "P"});
-          }
-        }
-      } else if (messageType == "P") {
-        if (!messagesP.has(k)) {
-          messagesP.set(k, []);
-        }
-        messagesP.get(k)!.push(x);
-        let messageP = messagesP.get(k)!;
-        if (messageP.length >= N - F) {
-          let count0 = messageP.filter((el:number) => el == 0).length;
-          let count1 = messageP.filter((el:number) => el == 1).length;
-
-          if (count0 >= F + 1) {
-            nodeState.x = 0;
-            nodeState.decided = true;
-          } else if (count1 >= F + 1) {
-            nodeState.x = 1;
-            nodeState.decided = true;
-          } else {
-            if (count0 + count1 > 0 && count0 > count1) {
-              nodeState.x = 0;
-            } else if (count0 + count1 > 0 && count0 < count1) {
-              nodeState.x = 1;
-            } else {
-              nodeState.x = Math.random() > 0.5 ? 0 : 1;
-            }
-            nodeState.k = k + 1;
-            for (let i = 0; i < N; i++) {
-              axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k + 1, x: nodeState.x, messageType: "R"});
-            }
-          }
-        }
-      }
-    }
-    res.status(200).send("message received");
-  });
-
   // TODO implement this
   // this route is used to start the consensus algorithm
   // node.get("/start", async (req, res) => {});
@@ -110,25 +53,6 @@ export async function node(
     while (!nodesAreReady()) {
       await delay(5);
     }
-
-    if (!isFaulty) {
-      nodeState.decided = false;
-      nodeState.x = initialValue;
-      nodeState.k = 1;
-      for (let i = 0; i < N; i++) {
-        await axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {
-          k: nodeState.k,
-          x: nodeState.x,
-          messageType: "R"
-        });
-      }
-    }
-    else {
-      nodeState.decided = null;
-      nodeState.x = null;
-      nodeState.k = null;
-    }
-    res.status(200).send("started");
   });
 
   // TODO implement this
